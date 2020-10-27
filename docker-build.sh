f21014b769b469322d4e0a70e9eaa36139e35bb3
Gary McWilliams <Gary.McWilliams@verint.com>
2020-10-27 19:30:10 +0000

    docker adds git_hash

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export GIT_HASH=$(git rev-parse HEAD)
 docker build --build-arg GIT_HASH=${GIT_HASH::7} -t ggbowlscalendar-app .
 docker tag ggbowlscalendar-app:latest garymcwilliams/ggbowlscalendar-app:latest
 docker push garymcwilliams/ggbowlscalendar-app:latest
