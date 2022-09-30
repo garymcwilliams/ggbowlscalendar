630a301f6ebbeb409d2454f356284234c5d9b29e
Gary McWilliams <garymcwilliams@gmail.com>
2022-09-30 14:10:29 +0100

    docker-build does not auto push to github

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,4 @@
 
 docker build -t ggbowlscalendar-app .
 docker tag ggbowlscalendar-app:latest garymcwilliams/ggbowlscalendar-app:latest
-docker push garymcwilliams/ggbowlscalendar-app:latest
+#docker push garymcwilliams/ggbowlscalendar-app:latest
