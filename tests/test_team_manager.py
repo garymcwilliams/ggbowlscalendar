cb284b0506a83cbfbebd23b62fc32312cbd3b5b6
Gary McWilliams <garymcwilliams@gmail.com>
2023-06-23 16:30:59 +0100

    test for unknown team added WIP #43

--- tests/test_team_manager.py
+++ tests/test_team_manager.py
@@ -3,8 +3,8 @@ test
 """
 from ggbowlscalendar.team_manager import TeamManager
 
-DATE1 = '2023-04-22'
-DATE2 = '2023-04-30'
+DATE_230402 = '2023-04-22'
+DATE_230430 = '2023-04-30'
 
 
 class TestTeamManager:
@@ -29,3 +29,14 @@ class TestTeamManager:
         assert team_manager.get_team_details('CLIFT')['name'] == 'clift'
         assert team_manager.get_team_details('CLIFT')['location'] \
             == 'clift location'
+
+    def test_unknown_team(self):
+        """test unknown team process"""
+        team_dict = {
+            'FALLSA': {'name': 'AAAA', 'location': 'AAA location'},
+            'CLIFT': {'name': 'clift', 'location': 'clift location'}
+        }
+
+        team_manager = TeamManager.from_dict(team_dict)
+        assert team_manager.get_team_details('XYZZY')['name'] == '***XYZZY***'
+        assert team_manager.get_team_details('XYZZY')['location'] == 'TBD'
