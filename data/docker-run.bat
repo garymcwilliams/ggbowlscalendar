2aba3746976dbdb5b369880f8b5bf8a556e6c396
Gary McWilliams <garymcwilliams@gmail.com>
2024-04-29 11:30:49 +0100

    Updated pre-commit WIP #62

--- data/docker-run.bat
+++ data/docker-run.bat
@@ -8,7 +8,7 @@ if "%ICAL_TEAM%"=="" goto no_team
 docker run -p 5000:5000 -e "ICAL_TEAM=%ICAL_TEAM%" -e "ICAL_YEAR=2022" -e "ICAL_OUTPUT=/opt/ics-data" -v "D:/Users/gmcwilliams/OneDrive/Documents/personal:/opt/ics-data" garymcwilliams/ggbowlscalendar
 
 ::allow dynamic mount of app-data, HOWEVER we prefer to rebuild the image, with data in place on each edit
-::-e "ICAL_DATAPATH=/app-data" -v "d:/dev/gitrepos/ggbowlscalendar-data:/app-data"  
+::-e "ICAL_DATAPATH=/app-data" -v "d:/dev/gitrepos/ggbowlscalendar-data:/app-data"
 
 ::debug, gets a terminal to allow inspecting of the image contents
 ::docker run -p 5000:5000 -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2018-19" -e "ICAL_OUTPUT=/opt/ics-data" -v "C:/Users/gmcwilliams/Dropbox:/opt/ics-data" ggbowlscalendar /bin/bash
--- docker-run.bat
+++ docker-run.bat
@@ -1,3 +1,2 @@
 docker run -p 5000:5000 -e "ICAL_DATAPATH=d:/dev/gary/gitrepos/icalendar-data" -e "ICAL_OUTPUT=/app" -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2019-20" garymcwilliams/ggbowlscalendar-app
 ::docker run -p 5000:5000 -e "ICAL_OUTPUT=/opt/ics-data" -e "ICAL_TEAM=stcolmans" -e "ICAL_YEAR=2019-20" -v "D:/Users/gmcwilliams/OneDrive/Documents/personal:/opt/ics-data" --entrypoint="/bin/bash" garymcwilliams/ggbowlscalendar
-
